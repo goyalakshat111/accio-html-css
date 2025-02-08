10a5454a8eb01734310629751a83cec0ec74e505
Akshat Goyal <goyalakshat111@gmail.com>
2025-02-09 00:33:04 +0530

    querySelector

--- dom-1/script.js
+++ dom-1/script.js
@@ -1,14 +1,14 @@
 // selecting single h1
-// let heading = document.querySelector("h1");
+let heading = document.querySelector("h1");
 
 // selecting multiple h1
-// let head = document.querySelectorAll("h1");
+let head = document.querySelectorAll("h1");
 
 // selecting by id
-// let para = document.querySelector("#kuch");
+let para = document.querySelector("#kuch");
 
 // selecting by class
-// let x = document.querySelectorAll('.item');
+let x = document.querySelectorAll('.item');
 
 
 // innerText - to change content inside opening and closing tag to another text
@@ -18,19 +18,24 @@ para.innerText = para.innerText + " bolta h";
 
 // para.innerHTML = "gg";
 //or
-// para.innerHTML = "<span>Akshat</span> <a href='#'>Google</a>";
+// para.innerHTML = "<a href='#'>Google</a>";
 
 // example 2 - innerHTML
 let box = document.querySelector("div");
 
+
+// try in REPL
 // console.log(box.innerText);
 // console.log(box.innerHTML);
 
 // input 
 let inp = document.querySelector("input");
+
+// try in REPL
 // console.log(inp.id);
 // console.log(inp.name);
 // console.log(inp.className);
+// console.log(inp.type);
 // console.log(inp.value);
 
 // inp.type = "password";
@@ -39,13 +44,13 @@ let inp = document.querySelector("input");
 // style
 let para2 = document.querySelector("#para");
 
-// way 1 - dom way
+// way 1 - using dom properties
 
 // para2.style.color = 'red';
 // para2.style.backgroundColor = 'yellow';
 // para2.style.fontSize = '40px';
 
-// way2 - like css
+// way2 - same as css syntax using cssText
 
 // para2.style.cssText = `
 // color:red;
@@ -53,14 +58,20 @@ let para2 = document.querySelector("#para");
 // font-size:40px;
 // `
 
-// way-3 - external css
+// way-3 - using external css file
 
+
+// class attribute in dom
+
+// adding class
 para2.className+= " design-para";
-// or
 
 // print all class
-// console.log(para.classList);
+console.log(para2.classList);
+
+// another way to add and remove class
+
 // add class
-// para.classList.add("design-para")
+// para2.classList.add("design-para")
 // remove class
-// para.classList.remove("design-para")
+// para2.classList.remove("design-para")
